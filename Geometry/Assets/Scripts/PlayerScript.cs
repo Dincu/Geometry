b97fe6baa7887b519a07d53263876d58d4d764c9
Vladimir de la Cruz <vladimirdlc@gmail.com>
2014-01-25 15:02:33 -0430

    Base example of figures movement via script new impl

--- Geometry/Assets/Scripts/PlayerScript.cs
+++ Geometry/Assets/Scripts/PlayerScript.cs
@@ -5,19 +5,46 @@ using System.Collections;
 /// </summary>
 public class PlayerScript : MonoBehaviour
 {
+    public float step = 30;
+    public playerPosition currentStance = playerPosition.CENTER;
+    public float damping;
+    private Vector3 originPosition;
+    private Vector3 targetPosition;
 
+    public void Start()
+    {
+        originPosition = transform.position;
+    }
+
+    public enum playerPosition
+    {
+        LEFT = -20,
+        CENTER = 0,
+        RIGTH = 20
+    }
 	
 	void Update()
 	{
 		if(Input.GetKeyDown(KeyCode.A))
 		{
-			animation.Play("move_left");
+            if (currentStance == playerPosition.RIGTH)
+            {
+                currentStance = playerPosition.CENTER;
+                targetPosition = originPosition;
+            }
+            else if (currentStance == playerPosition.CENTER)
+            {
+                currentStance = playerPosition.LEFT;
+                targetPosition = originPosition + new Vector3((float)playerPosition.LEFT, 0);
+            }
 		}
 
 		if(Input.GetKeyDown(KeyCode.D))
 		{
-			animation.Play("move_right");
+
 		}
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * damping);
 	}
 	
 	void FixedUpdate()
