e6b7a2d7f75a02696a500d7bf875c09ffc4ff7fd
Dincu <dgmarcellab@gmail.com>
2014-01-25 20:56:46 -0430

    Right sphere rotation
    Rotation rotates for the right.

--- Geometry/Assets/Scripts/RailController.cs
+++ Geometry/Assets/Scripts/RailController.cs
@@ -42,6 +42,7 @@ public class RailController : MonoBehaviour {
     {
         GameObject clone = Instantiate(shapes[Random.Range(0, shapes.Length)]) as GameObject;
 
+
 		clone.animation.Play("Falling"); 
 		clone.animation["SphereRight"].layer = 1; 
 		clone.animation.Play("SphereRight"); 
